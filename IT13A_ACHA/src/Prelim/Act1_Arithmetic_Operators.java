ec76d2c4a37d4714fa9f651f3529b46246eb90e0
huntznavarro10 <achajohnryan@gmail.com>
2025-02-13 23:15:36 +0800

    activity 1
    Added Java Application

---
+++ IT13A_ACHA/src/Prelim/Act1_Arithmetic_Operators.java
@@ -0,0 +1,29 @@
+
+package Prelim;
+
+
+public class Act1_Arithmetic_Operators {
+     public static void main(String[] org){
+             
+        int numb1 = 10;
+        int numb2 = 6;
+        int numb3 = 4;
+        int numb4 = 3;
+        
+        
+        
+        int low1 = numb1 * numb3 + numb2;
+        int depend1 = numb1 - numb3;
+        int low2 = depend1 % numb2;
+        int depend2 = numb1 + numb3 + numb2;
+        int low3 = depend2 / numb4;
+        int depend3 = numb3 * numb3;
+        int low4 = numb1 * numb2 - depend3;
+        
+          System.out.println("10 * 4 + 6 ="+low1);
+         System.out.println("(10-4) % 6 = "+low2);
+            System.out.println("(10 + 4 + 6)/3 = "+low3);
+               System.out.println("10 * 6 -(4 * 4 ) = "+low4);
+               
+     }
+}
---
+++ IT13A_ACHA/src/it13a_acha/IT13A_ACHA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package it13a_acha;
+
+/**
+ *
+ * @author Administrator
+ */
+public class IT13A_ACHA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
